0574079d2fa08273b1e2af504ba9f2339df9f027
eeeseohyun <eeeseohyun@gmail.com>
2023-01-20 18:51:28 +0900

    JavaBasic2 : 데이터와 연산자

---
+++ JavaBasic2/JavaBasic2_3.java
@@ -0,0 +1,17 @@
+
+public class JavaBasic2_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int nVar1, nVar2, nResult;
+		nVar1 =5;
+		nVar2 = 6;
+		nResult = nVar1 + nVar2;
+		
+		System.out.println(nResult);
+		System.out.println(12 > 4);
+		System.out.println(12 == 4);
+		System.out.println((12 > 4) && (12 == 4) );
+	}
+
+}
