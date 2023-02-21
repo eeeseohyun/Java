1e46809d4293cfacb8e730deeacc6324d3af0a68
eeeseohyun <eeeseohyun@gmail.com>
2023-02-21 22:50:04 +0900

    javaBasic1 : 표준출력

---
+++ JavaBasic1/JavaBasic1_2.java
@@ -0,0 +1,16 @@
+
+public class JavaBasic1_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int nVar=12;
+		float fVar=3.14f;
+		
+		System.out.println(nVar);
+		System.out.println(fVar);
+		System.out.println();
+		System.out.println("정수: "+nVar+" 부동소수: " +fVar);
+		System.out.printf("정수: %1$d 부동소수 %2$f", nVar, fVar);
+	}
+
+}
